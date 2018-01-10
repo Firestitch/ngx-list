a5669a6370ace6ee73083e758978e9632a640e7c
Raymond <ray@firestitch.com>
2018-01-10 10:51:56 -0500

    initial commit

---
+++ playground/welcome.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  template: '<h1>Welcome!</h1>',
+  styles: []
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
