24c1a67b1cbc40989de0e356c50925b8e7c58230
Raymond Gigliotti <ray@firestitch.com>
2024-12-11 11:46:55 -0500

    switch many paging status to current page number only

--- playground/app/components/kitchensink/kitchensink.component.ts
+++ playground/app/components/kitchensink/kitchensink.component.ts
@@ -67,7 +67,7 @@ export class KitchenSinkComponent
       queryParam: true,
       paging: {
         limits: [5, 15, 50, 150, 250, 500, 1000],
-        strategy: PaginationStrategy.Offset,
+        strategy: PaginationStrategy.Many,
       },
       rowHoverHighlight: true,
       // sort: {
--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -2,11 +2,8 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
-  OnDestroy,
-  OnInit,
 } from '@angular/core';
 
-import { BehaviorSubject, filter, Observable, Subject, takeUntil, tap } from 'rxjs';
 
 import { PaginationController } from '../../classes/pagination-controller';
 
@@ -16,52 +13,24 @@ import { PaginationController } from '../../classes/pagination-controller';
   styleUrls: ['./pagination.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FsPaginationComponent implements OnInit, OnDestroy {
+export class FsPaginationComponent {
 
   @Input() public pagination: PaginationController;
   @Input() public rows;
-  @Input() public loading$: Observable<boolean>;
-
-  private _destroy$ = new Subject<void>();
-  private _paging$ = new BehaviorSubject<boolean>(false);
-
-  public ngOnInit(): void {
-    this.loading$
-      .pipe(
-        filter((loading) => !loading),
-        tap(() => this._paging$.next(false)),
-        takeUntil(this._destroy$),
-      )
-      .subscribe();
-  }
-
-  public get paging$(): Observable<boolean> {
-    return this._paging$.asObservable();
-  }
 
   public first() {
-    this._paging$.next(true);
     this.pagination.goFirst();
   }
 
   public prev() {
-    this._paging$.next(true);
     this.pagination.goPrev();
   }
 
   public next() {
-    this._paging$.next(true);
     this.pagination.goNext();
   }
 
   public last() {
-    this._paging$.next(true);
     this.pagination.goLast();
   }
-
-  public ngOnDestroy(): void {
-    this._destroy$.next();
-    this._destroy$.complete();
-  }
-
 }
