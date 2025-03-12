89a7ca534f76f35a6c644641deb9dfb7713d3e5d
Raymond Gigliotti <ray@firestitch.com>
2025-03-12 08:46:13 -0400

    added ngTemplateContextGuard to row cell

--- src/app/directives/cell/cell.directive.ts
+++ src/app/directives/cell/cell.directive.ts
@@ -1,8 +1,26 @@
 import { Directive, Input } from '@angular/core';
 
+
 @Directive({ selector: '[fs-list-cell]' })
 export class FsListCellDirective {
+
   @Input() public colspan;
   @Input() public align: string;
   @Input('class') public className: string | string[];
+
+  public static ngTemplateContextGuard(
+    directive: FsListCellDirective,
+    context: unknown,
+  ): context is {
+    $implicit: any,
+    row: any,
+    index: number,
+    group: any,
+    groupRow: any,
+    groupIndex: number,
+    groupChildren: any[],
+    expanded: boolean,
+  } {
+    return true;
+  }
 }
